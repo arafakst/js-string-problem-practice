f41c1f4d68e0b922af8de03823d9c3c284acb1ec
arafakst <arafakst@gmail.com>
2023-03-08 22:37:31 +0600

    case

--- case.js
+++ case.js
@@ -1,2 +1,9 @@
-const userName = 'blackoink';
-const iserInput = 'blackpink';
+const userName = 'blackpink';
+const userInput = 'blackpinK';
+console.log(userInput.toUpperCase())
+if(userName.toLowerCase() === userInput.toLocaleLowerCase()){
+    console.log('Valid User')
+}
+else{
+    console.log('Invalid User')
+}
